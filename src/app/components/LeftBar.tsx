f6504c857de8009ee8cdf6d7c9ae053ce55e2d40
Rahul Yadav <rahulyadav989290@gmail.com>
2025-01-11 11:36:46 +0530

    implementing the left side wit icons

--- src/app/components/LeftBar.tsx
+++ src/app/components/LeftBar.tsx
@@ -1,7 +1,131 @@
-import React from "react";
+import Link from "next/link";
+import Image from "./Image";
+
+const menuList = [
+  {
+    id: 1,
+    name: "Homepage",
+    link: "/",
+    icon: "home.svg",
+  },
+  {
+    id: 2,
+    name: "Explore",
+    link: "/",
+    icon: "explore.svg",
+  },
+  {
+    id: 3,
+    name: "Notification",
+    link: "/",
+    icon: "notification.svg",
+  },
+  {
+    id: 4,
+    name: "Messages",
+    link: "/",
+    icon: "message.svg",
+  },
+  {
+    id: 5,
+    name: "Bookmarks",
+    link: "/",
+    icon: "bookmark.svg",
+  },
+  {
+    id: 6,
+    name: "Jobs",
+    link: "/",
+    icon: "job.svg",
+  },
+  {
+    id: 7,
+    name: "Communities",
+    link: "/",
+    icon: "community.svg",
+  },
+  {
+    id: 8,
+    name: "Premium",
+    link: "/",
+    icon: "logo.svg",
+  },
+  {
+    id: 9,
+    name: "Profile",
+    link: "/",
+    icon: "profile.svg",
+  },
+  {
+    id: 10,
+    name: "More",
+    link: "/",
+    icon: "more.svg",
+  },
+];
 
 const LeftBar = () => {
-  return <div>LeftBar</div>;
+  return (
+    <div className="h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8">
+      {/* LOGO MENU BUTTON */}
+      <div className="flex flex-col gap-4 text-lg items-center 2xl:items-start">
+        {/* LOGO */}
+        <Link href="/" className="p-2 rounded-full hover:bg-[#181818] ">
+          <Image path="public/icons/logo.svg" alt="logo" w={24} h={24} />
+        </Link>
+        {/* MENU LIST */}
+        <div className="flex flex-col gap-4">
+          {menuList.map((item) => (
+            <Link
+              href={item.link}
+              className="p-2 rounded-full hover:bg-[#181818] flex items-center gap-4"
+              key={item.id}
+            >
+              <Image
+                path={`public/icons/${item.icon}`}
+                alt={item.name}
+                w={24}
+                h={24}
+              />
+              <span className="hidden 2xl:inline">{item.name}</span>
+            </Link>
+          ))}
+        </div>
+        {/* BUTTON */}
+        <Link
+          href="/"
+          className="bg-white text-black rounded-full w-12 h-12 flex items-center justify-center 2xl:hidden"
+        >
+          <Image path="public/icons/post.svg" alt="new post" w={24} h={24} />
+        </Link>
+        <Link
+          href="/compose/post"
+          className="hidden 2xl:block bg-white text-black rounded-full font-bold py-2 px-20"
+        >
+          Post
+        </Link>
+      </div>
+      {/* USER */}
+      <div className="flex items-center justify-between ">
+        <div className="flex items-center gap-2">
+          <div className="w-10 h-10 relative rounded-full overflow-hidden">
+            <Image
+              path="public/general/avatar.png"
+              alt="rahul yadav"
+              w={100}
+              h={100}
+              tr={true}
+            />
+          </div>
+          <div className="hidden 2xl:flex flex-col">
+            <span className="font-bold">Rahul yadav</span>
+            <span className="text-sm text-textGray">@Rahul</span>
+          </div>
+        </div>
+        <div className="hidden 2xl:block cursor-pointer font-bold">...</div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftBar;
